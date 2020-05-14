139e096e884e9351a82ab007413d4b16f103764f
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-05-14 09:32:14 +0200

    Ng For, Style et NgIf

--- src/app/scientifiques/scientifiques.component.ts
+++ src/app/scientifiques/scientifiques.component.ts
@@ -10,6 +10,24 @@ export class ScientifiquesComponent implements OnInit {
   title = "Détail sur ";
   toto;
   exemple;
+
+  personnes = [{
+                nom : "Einstein",
+                prenom : "Albert",
+                decouverte : "E=MC²",
+                description : "The scientifique"
+              },{
+                nom : "Curie",
+                prenom : "Marie",
+                decouverte : "Radioactivité",
+                description : ""
+              },{
+                nom : "Curie",
+                prenom : "Pierre",
+                decouverte : "Radioactivité",
+                description : ""
+              }];
+
   personne = {
                 nom : "Einstein",
                 prenom : "Albert",
@@ -24,6 +42,14 @@ export class ScientifiquesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  presse(e){
+    console.log(this);
+    console.log(e);
+  }
+
+  maCondition(){
+    return this.personne.nom=='Einstein';
+  }
 
 
 }

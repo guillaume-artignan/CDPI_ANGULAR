a4d09f9eeb4d8abb12f111176e22e67475f35581
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-05-14 08:35:49 +0200

    Calculatrice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ScientifiquesComponent } from './scientifiques/scientifiques.component';
+import { CalcultriceComponent } from './calcultrice/calcultrice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    ScientifiquesComponent
+    ScientifiquesComponent,
+    CalcultriceComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [ScientifiquesComponent]
+  bootstrap: [CalcultriceComponent]
 })
 export class AppModule { }
---
+++ src/app/calcultrice/calcultrice.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './calcultrice.component.html',
+  styleUrls: ['./calcultrice.component.css']
+})
+export class CalcultriceComponent implements OnInit {
+
+  resultat = "0";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  egal(){
+    try{
+      this.resultat = ""+eval(this.resultat);
+    } catch (e){
+      this.resultat = "Err";
+    }
+  }
+
+  nettoie(){
+    this.resultat = "0";
+  }
+
+  presse(e,touche){
+    if (this.resultat=="0" || this.resultat=="Err")
+      this.resultat=""+touche;
+    else this.resultat+=""+touche;
+  }
+
+}
